581ad31d17975b21bd79fe28bbb6c888fcd2990c
A.Ukolov <sanjrostov@mail.ru>
2022-01-10 08:07:41 +0300

    Initial Commit

---
+++ DZ_1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ_1.1
+//
+//  Created by Александр Уколов on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
